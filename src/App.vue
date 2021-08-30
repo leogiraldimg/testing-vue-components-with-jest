6caf7847af498f2c319658373f0404106e9f1b28
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-08-30 07:06:44 -0300

    :rotating_light: applies prettier

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,36 @@
 <template>
-
-    <div class="container mt-5">
-
-        <Chat :handleSubmitChatMessage="this.handleSubmitChatMessage" :messages="this.$data.messages"/>
-
-    </div>
-
+  <div class="container mt-5">
+    <Chat
+      :handleSubmitChatMessage="this.handleSubmitChatMessage"
+      :messages="this.$data.messages"
+    />
+  </div>
 </template>
 
 <script>
+import { ChatMessageService } from "./services/chatMessageService.js";
 
-    import { ChatMessageService } from './services/chatMessageService.js';
+export default {
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  async mounted() {
+    const messages = await ChatMessageService.getMessages();
+    this.$data.messages.push(...messages);
+  },
+  methods: {
+    async handleSubmitChatMessage(content) {
+      const message = {
+        content,
+        sender: "YOU",
+      };
 
-    export default {
+      await ChatMessageService.submitMessage(message);
 
-        data(){
-
-            return {
-
-                messages: []
-            }
-
-        },
-        async mounted(){
-
-            const messages = await ChatMessageService.getMessages();
-            this.$data.messages.push(... messages);
-
-        },
-        methods:{
-
-            async handleSubmitChatMessage(content){
-
-                const message = {
-                    content,
-                    sender: "YOU"
-                }
-
-                await ChatMessageService.submitMessage(message);
-
-                this.$data.messages.push(message);
-
-            }
-
-        }
-
-    }
-
+      this.$data.messages.push(message);
+    },
+  },
+};
 </script>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import Vue from 'vue';
+import Vue from "vue";
 import App from "./App.vue";
-import Chat from './routes/Chat/Chat.vue';
+import Chat from "./routes/Chat/Chat.vue";
 
-const chatComponent = Vue.component('Chat',Chat);
+const chatComponent = Vue.component("Chat", Chat);
 
-new Vue({
-
-        el:"#App",
-        render: h => h(App),
-        components:{
-
-            "Chat":chatComponent
-
-        }
-        // ChatMessageService
-        
+new Vue(
+  {
+    el: "#App",
+    render: (h) => h(App),
+    components: {
+      Chat: chatComponent,
     },
-    //{ second argument is loosely documented at best
-      // ChatMessageService
-    //}
-
+    // ChatMessageService
+  }
+  //{ second argument is loosely documented at best
+  // ChatMessageService
+  //}
 );
