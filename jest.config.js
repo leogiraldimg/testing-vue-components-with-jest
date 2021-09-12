dcc63e9bbfe2865d94e69a258c7251f7fe1db372
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-09-12 20:28:16 -0300

    :package: includes jest dependencies

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
+};
