d963f3153661fc162fe37d494c459a2e5d1ab19c
leogiraldimg@gmail.com <leogiraldimg@gmail.com>
2021-09-21 08:50:48 -0300

    :sparkles: tests asynchronous methods in vue applications

--- src/services/chatMessageService/chatMessageService.spec.js
+++ src/services/chatMessageService/chatMessageService.spec.js
@@ -1,3 +1,22 @@
+import { ChatMessageService } from './';
+
 describe('the Chat Message Service', () => {
-  it.todo('should return an array of messages');
+  let chatMessageService;
+  
+  beforeEach(() => {
+    chatMessageService = ChatMessageService([]);
+  });
+
+  it('should add messages', async () => {
+    const content = 'Thank you for using Vue!';
+
+    expect(await chatMessageService.getMessages()).toHaveLength(0);
+
+    await chatMessageService.submitMessage({ content });
+
+    expect(await chatMessageService.getMessages()).toHaveLength(1);
+
+    let messages = await chatMessageService.getMessages();
+    expect(messages[0].content).toEqual(content);
+  });
 });
